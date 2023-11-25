0d949e78cde334443a54de4828b35024650481c2
GustavoTrindade16 <a85492@ualg.pt>
2023-11-25 00:04:34 +0000

    informação sobre a loja

---
+++ clientes-cria_novo_cliente.py
@@ -0,0 +1,9 @@
+def cria_novo_cliente():
+    """Pedir os dados de um novo cliente
+
+    :return: dicionario com o novo cliente, {"nome": <<nome>>, "nif": <<nif>>, ...}
+    """
+    # TODO: pedir os dados do cliente e não esquecer de os devolver
+    # ...
+
+    pass
---
+++ veiculos.py
@@ -0,0 +1,12 @@
+from io_terminal import imprime_lista
+
+nome_ficheiro_lista_de_veiculos = "lista_de_veiculos.pk"
+
+
+# TODO: Copie para aqui o código de cada uma das funções nos
+# ficheiros com o nome veiculos-*.py e faça um commit de cada vez
+# Quando este ficheiro estiver completo com todas as suas funções,
+# deve ser o unico ficheiro veiculos.py existente, deve apagar
+# todos os outros ficheiros veiculos-*.py, e inclusive estes comentários
+
+# ...
