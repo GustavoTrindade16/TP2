a80252a39955bcc4ac2f9d6efc7b659b832deb96
GustavoTrindade16 <a85492@ualg.pt>
2023-12-01 20:51:51 +0000

    alterção do sistemas veicululos

--- Programa/veiculos.py
+++ Programa/veiculos.py
@@ -3,10 +3,8 @@ from io_terminal import imprime_lista
 nome_ficheiro_lista_de_veiculos = "lista_de_veiculos.pk"
 
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome veiculos-*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro veiculos.py existente, deve apagar
-# todos os outros ficheiros veiculos-*.py, e inclusive estes comentários
+def imprime_lista_de_veiculos(lista_de_veiculos):
+    """TODO: documentação"""
 
-# ...
+    imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
+    
