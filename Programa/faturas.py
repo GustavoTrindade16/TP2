63373d9e3600d7b5ad85c00352ad1111f295264e
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-01 19:03:01 +0000

    Adicionei uma função def no codigo

--- Programa/faturas.py
+++ Programa/faturas.py
@@ -12,3 +12,22 @@ nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
 # todos os outros ficheiros faturas-*.py, e inclusive estes comentários
 
 # ...
+
+def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
+    """Pede ao utilizador para introduzir os dados de uma nova fatura
+
+    :return: dicionario com uma fatura, na forma
+        {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
+    """
+
+    id_cliente = pergunta_id(questao="Qual o id do cliente?", lista=lista_de_clientes, mostra_lista=True)
+    id_veiculo = pergunta_id(questao="Qual o id do veiculo?", lista=lista_de_veiculos, mostra_lista=True)
+
+    # TODO: Pedir o resto dos dados da fatura, e não esquecer de os guardar no dicionario
+    # ...
+
+    fatura = {"cliente": id_cliente,
+              "veiculo": id_veiculo,
+              "data": date.today()}
+
+    return fatura
