d4c510a76aad5a3a95f51556bcbaa3f9bcbc44ed
GustavoTrindade16 <a85492@ualg.pt>
2023-12-01 18:26:27 +0000

    Remover arquivo clientes-imprime_lista_de_clientes.py

--- Programa/clientes-cria_novo_cliente.py
+++
@@ -1,9 +0,0 @@
-def cria_novo_cliente():
-    """Pedir os dados de um novo cliente
-
-    :return: dicionario com o novo cliente, {"nome": <<nome>>, "nif": <<nif>>, ...}
-    """
-    # TODO: pedir os dados do cliente e não esquecer de os devolver
-    # ...
-
-    pass
