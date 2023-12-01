a921cf75f52b201cf65c57c80bd69a5a7fae8a8d
GustavoTrindade16 <a85492@ualg.pt>
2023-12-01 18:19:29 +0000

    atualização adicionando a função adicionar cliente

--- Programa/clientes.py
+++ Programa/clientes.py
@@ -2,9 +2,18 @@ from io_terminal import imprime_lista
 
 nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
 
+def cria_novo_cliente():
+    """Pedir os dados de um novo cliente
+
+    return: dicionario com o novo cliente, {"nome": <<nome>>, "nif": <<nif>>, ...}
+    """
+    # TODO: pedir os dados do cliente e não esquecer de os devolver
+    # ...
+
+    pass
+
+
 def imprime_lista_de_clientes(lista_de_clientes):
     """TODO: documentação"""
 
-    #TODO: Implementar esta função
-    # ...
 
