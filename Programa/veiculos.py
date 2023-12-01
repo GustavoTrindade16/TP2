b9a25af20da27e6ea1416e94c75f58a022a22813
GustavoTrindade16 <a85492@ualg.pt>
2023-12-01 21:26:15 +0000

    alteração do codigo veiculos e add veiculos-criados

--- Programa/veiculos.py
+++ Programa/veiculos.py
@@ -7,4 +7,20 @@ def imprime_lista_de_veiculos(lista_de_veiculos):
     """TODO: documentação"""
 
     imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
-    
+
+def cria_novo_veiculo():
+    """Pede ao utilizador para introduzir um novo veiculo
+
+    :return: dicionario com um veiculo na forma
+        {"marca": <<marca>>, "matricula": <<matricula>>, ...}
+    """
+
+    marca = input("marca? ")
+    matricula = input("matricula? ").upper()
+    # TODO: Pedir o resto dos dados do veiculo, e não esquecer de os guardar no dicionario
+    # ...
+
+    veiculo = {"marca": marca,
+               "matricula": matricula}
+
+    return veiculo
