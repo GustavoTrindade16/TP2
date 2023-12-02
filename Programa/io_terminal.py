fdacd56a4979c6f71cf961e76cf745512d9cada5
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-02 01:01:51 +0000

    Alteração do código io_terminal.py

--- Programa/io_terminal.py
+++ Programa/io_terminal.py
@@ -1,6 +1,5 @@
 from tabulate import tabulate
 
-
 # TODO: Copie para aqui o código de cada uma das funções nos
 # ficheiros com o nome io_terminal*.py e faça um commit de cada vez
 # Quando este ficheiro estiver completo com todas as suas funções,
@@ -8,3 +7,35 @@ from tabulate import tabulate
 # todos os outros ficheiros io_terminal-*.py, e inclusive estes comentários
 
 # ...
+
+def imprime_lista(cabecalho, lista):
+    """Imprime a :attr:`lista` na forma de uma tabela com um cabeçalho
+
+    Recebe uma lista na forma [{"atrib1": valor1, "atrib2": valor2, ...},
+    {"atrib1": valor1, "atrib2": valor2, ...}, ...] e imprime no terminal uma tabela
+    na forma
+
+    ==  ======  ======
+    id  atrib1  atrib2
+    ==  ======  ======
+    0   valor1  valor2
+    1   ...      ...
+    ==  ======  ======
+
+    :param cabecalho: Cabecalho para a listagem
+    :param lista: Lista a ser impressa
+    """
+
+    print(cabecalho)
+
+    if (len(lista) == 0):
+        print("Lista vazia")
+    else:
+        # cabecalho da tabela
+        lista_a_imprimir = [["id"] + list(lista[0].keys())]
+        # dados
+        lista_a_imprimir.extend([[id] + list(d.values()) for id, d in enumerate(lista)])
+
+        print(tabulate(lista_a_imprimir, headers="firstrow", tablefmt='psql'))
+
+
