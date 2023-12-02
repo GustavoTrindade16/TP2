586443159c7efd16db4d4bfdd14510ee3437e5c3
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-02 02:11:57 +0000

    Alteração do código io_ficheiros.py

--- Programa/io_ficheiros.py
+++ Programa/io_ficheiros.py
@@ -13,3 +13,12 @@ from veiculos import nome_ficheiro_lista_de_veiculos
 # todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
 
 # ...
+
+def carrega_as_listas_dos_ficheiros():
+    """TODO: documentação"""
+
+    lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_veiculos)
+    lista_de_clientes = le_de_ficheiro(nome_ficheiro_lista_de_clientes)
+    lista_de_faturas = le_de_ficheiro(nome_ficheiro_lista_de_faturas)
+
+    return  lista_de_veiculos, lista_de_clientes, lista_de_faturas
