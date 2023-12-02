5d7483bb238e0bf6fd13de9cabd768038bb405b6
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-02 01:13:18 +0000

    "Alteração do código io_terminal.py"

--- Programa/io_terminal.py
+++ Programa/io_terminal.py
@@ -38,10 +38,30 @@ def imprime_lista(cabecalho, lista):
 
         print(tabulate(lista_a_imprimir, headers="firstrow", tablefmt='psql'))
 
-    def pause():
-        """Faz uma pausa no programa e espera que o utilizador pressione ENTER"""
+def pause():
 
-        input("Pressione ENTER para continuar...")
+    """Faz uma pausa no programa e espera que o utilizador pressione ENTER"""
+
+    input("Pressione ENTER para continuar...")
+
+def pergunta_id(questao, lista, mostra_lista=False):
+    """TODO: documentação
+
+    :param questao:
+    :param lista:
+    :param mostra_lista:
+    :return:
+    """
+
+    if mostra_lista:
+        imprime_lista(cabecalho="", lista=lista)
+
+    while True:
+        id = int(input(questao))
+        if 0 <= id < len(lista):
+            return id
+        else:
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
 
     
 
