daf6757cdc8db1e2f496fbc8e5b75190b08a723e
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-02 02:17:14 +0000

    Remoção dos ficheiros io_terminal.py

--- Programa/io_ficheiros-le_de_ficheiro.py
+++
@@ -1,9 +0,0 @@
-def le_de_ficheiro(nome_ficheiro):
-    """Lê os dados de um ficheiro
-
-    :param nome_ficheiro: nome do ficheiro onde estao os dados
-    :return: o que leu do ficheiro (depende dos dados guardados)
-    """
-
-    with open(nome_ficheiro, "rb") as f:
-        return pickle.load(f)
