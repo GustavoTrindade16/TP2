3b0f5b8cc1c638e809fdbb271177548a394768c3
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-02 02:16:15 +0000

    Remoção da documentação dos ficheiros io_ficheiros.py

--- Programa/io_ficheiros.py
+++ Programa/io_ficheiros.py
@@ -15,7 +15,6 @@ from veiculos import nome_ficheiro_lista_de_veiculos
 # ...
 
 def carrega_as_listas_dos_ficheiros():
-    """TODO: documentação"""
 
     lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_veiculos)
     lista_de_clientes = le_de_ficheiro(nome_ficheiro_lista_de_clientes)
@@ -24,12 +23,6 @@ def carrega_as_listas_dos_ficheiros():
     return  lista_de_veiculos, lista_de_clientes, lista_de_faturas
 
 def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas):
-    """TODO: documentação
-
-    :param lista_de_clientes:
-    :param lista_de_veiculos:
-    :param lista_de_faturas:
-    """
 
     op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
     if op in ['s', 'S']:
@@ -40,21 +33,11 @@ def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de
         print("Gravação cancelada...")
 
 def guarda_em_ficheiro(nome_do_ficheiro, dados):
-    """Guarda os dados recebidos num ficheiro
-
-    :param nome_do_ficheiro: nome do ficheiro onde vai guardar os dados
-    :param dados: dados a serem guardados
-    """
 
     with open(nome_do_ficheiro, "wb") as f:
         pickle.dump(dados, f)
 
 def le_de_ficheiro(nome_ficheiro):
-    """Lê os dados de um ficheiro
-
-    :param nome_ficheiro: nome do ficheiro onde estao os dados
-    :return: o que leu do ficheiro (depende dos dados guardados)
-    """
 
     with open(nome_ficheiro, "rb") as f:
         return pickle.load(f)
