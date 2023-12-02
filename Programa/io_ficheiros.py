6428aa9576fef4656f7c87c822ada7c78b409aa6
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-02 02:15:10 +0000

    Alteração do código io_ficheiros.py

--- Programa/io_ficheiros.py
+++ Programa/io_ficheiros.py
@@ -49,3 +49,12 @@ def guarda_em_ficheiro(nome_do_ficheiro, dados):
     with open(nome_do_ficheiro, "wb") as f:
         pickle.dump(dados, f)
 
+def le_de_ficheiro(nome_ficheiro):
+    """Lê os dados de um ficheiro
+
+    :param nome_ficheiro: nome do ficheiro onde estao os dados
+    :return: o que leu do ficheiro (depende dos dados guardados)
+    """
+
+    with open(nome_ficheiro, "rb") as f:
+        return pickle.load(f)
