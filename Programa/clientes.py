c45f2e6a15ea45d4cb121a0affb83865d63e9e67
GustavoTrindade16 <a85492@ualg.pt>
2023-12-03 23:10:36 +0000

    adicionado a documentação para auxiliar no codigo faturas

--- Programa/clientes.py
+++ Programa/clientes.py
@@ -5,39 +5,6 @@ nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
 def cria_novo_cliente():
     """Cria um novo cliente solicitando seus dados.
 
-    :return: Dicionário com os dados do novo cliente {"nome": <<nome>>, "nif": <<nif>>, ...}
-    """
-    # TODO: Pedir os dados do cliente e não esquecer de devolvê-los
-    # ...
-
-    pass
-
-def imprime_lista_de_clientes(lista_de_clientes):
-    """Imprime a lista de clientes.
-
-    :param lista_de_clientes: Lista de clientes a ser impressa
-    """
-    # TODO: Implementar esta função
-    # ...
-Agora, temos um início de organização no código. O próximo passo seria preencher os TODOs com a implementação real das funções e detalhar a documentação da função imprime_lista_de_clientes. Se quiser, compartilhe mais detalhes sobre o que cada função deve fazer, e podemos continuar a trabalhar nisso.
-
-
-
-
-User
-poderia fazer a documentação mais completa
-ChatGPT
-Claro, vou expandir a documentação para ambas as funções:
-
-python
-Copy code
-from io_terminal import imprime_lista
-
-nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
-
-def cria_novo_cliente():
-    """Cria um novo cliente solicitando seus dados.
-
     Esta função interage com o usuário para coletar informações sobre um novo cliente,
     como nome, NIF, etc., e retorna um dicionário contendo esses dados.
 
@@ -65,3 +32,4 @@ def imprime_lista_de_clientes(lista_de_clientes):
 # Exemplo de uso:
 # lista_de_clientes = [...]  # Substitua isso com sua própria lista de clientes
 # imprime_lista_de_clientes(lista_de_clientes)
+
--- Programa/faturas.py
+++ Programa/faturas.py
@@ -1,36 +1,46 @@
 from datetime import date
-
 from io_terminal import imprime_lista, pergunta_id
 
 nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
 
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome faturas*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro faturas.py existente, deve apagar
-# todos os outros ficheiros faturas-*.py, e inclusive estes comentários
-
-# ...
 
 def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
-    """Pede ao utilizador para introduzir os dados de uma nova fatura
+    """Cria uma nova fatura solicitando os dados ao usuário.
 
-    :return: dicionario com uma fatura, na forma
-        {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
+    Esta função interage com o usuário para obter informações sobre uma nova fatura,
+    como o ID do cliente, o ID do veículo e a data da fatura. Retorna um dicionário
+    contendo esses dados.
+
+    :param lista_de_clientes: Lista de clientes disponíveis para seleção.
+    :param lista_de_veiculos: Lista de veículos disponíveis para seleção.
+    :return: Dicionário com os dados da nova fatura.
+             Exemplo: {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
     """
+    
 
     id_cliente = pergunta_id(questao="Qual o id do cliente?", lista=lista_de_clientes, mostra_lista=True)
-    id_veiculo = pergunta_id(questao="Qual o id do veiculo?", lista=lista_de_veiculos, mostra_lista=True)
+    id_veiculo = pergunta_id(questao="Qual o id do veículo?", lista=lista_de_veiculos, mostra_lista=True)
 
+    nova_fatura = {"cliente": id_cliente,
+                   "veiculo": id_veiculo,
+                   "data": date.today()}
 
-    fatura = {"cliente": id_cliente,
-              "veiculo": id_veiculo,
-              "data": date.today()}
-
-    return fatura
+    return nova_fatura
 
 def imprime_lista_de_faturas(lista_de_faturas):
-    """TODO: documentação"""
+    """Imprime a lista de faturas formatadamente.
 
-    pass
+    Esta função recebe uma lista de faturas e imprime seus dados de maneira formatada
+    no terminal.
+
+    :param lista_de_faturas: Lista de faturas a ser impressa.
+                            Cada fatura é representada como um dicionário.
+                            Exemplo: [{"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}, {...}, ...]
+    """
+    # TODO: Implementar esta função
+    # ...
+
+# Exemplo de uso:
+# lista_de_faturas = [...]  # Substitua isso com sua própria lista de faturas
+# imprime_lista_de_faturas(lista_de_faturas)
