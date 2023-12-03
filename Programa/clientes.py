b0cc9179607b5abb0844299628f641713b2eadbb
GustavoTrindade16 <a85492@ualg.pt>
2023-12-03 23:07:09 +0000

    adicionado a documentação para auxuliar no codigo

--- Programa/clientes.py
+++ Programa/clientes.py
@@ -3,17 +3,65 @@ from io_terminal import imprime_lista
 nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
 
 def cria_novo_cliente():
-    """Pedir os dados de um novo cliente
+    """Cria um novo cliente solicitando seus dados.
 
-    return: dicionario com o novo cliente, {"nome": <<nome>>, "nif": <<nif>>, ...}
+    :return: Dicionário com os dados do novo cliente {"nome": <<nome>>, "nif": <<nif>>, ...}
     """
-    # TODO: pedir os dados do cliente e não esquecer de os devolver
+    # TODO: Pedir os dados do cliente e não esquecer de devolvê-los
     # ...
 
     pass
 
+def imprime_lista_de_clientes(lista_de_clientes):
+    """Imprime a lista de clientes.
+
+    :param lista_de_clientes: Lista de clientes a ser impressa
+    """
+    # TODO: Implementar esta função
+    # ...
+Agora, temos um início de organização no código. O próximo passo seria preencher os TODOs com a implementação real das funções e detalhar a documentação da função imprime_lista_de_clientes. Se quiser, compartilhe mais detalhes sobre o que cada função deve fazer, e podemos continuar a trabalhar nisso.
+
+
+
+
+User
+poderia fazer a documentação mais completa
+ChatGPT
+Claro, vou expandir a documentação para ambas as funções:
+
+python
+Copy code
+from io_terminal import imprime_lista
+
+nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
+
+def cria_novo_cliente():
+    """Cria um novo cliente solicitando seus dados.
+
+    Esta função interage com o usuário para coletar informações sobre um novo cliente,
+    como nome, NIF, etc., e retorna um dicionário contendo esses dados.
+
+    :return: Dicionário com os dados do novo cliente.
+             Exemplo: {"nome": "João", "nif": "123456789", ...}
+    """
+    # TODO: Pedir os dados do cliente e não esquecer de devolvê-los
+    # ...
+
+    pass
 
 def imprime_lista_de_clientes(lista_de_clientes):
-    """TODO: documentação"""
+    """Imprime a lista de clientes formatadamente.
 
+    Esta função recebe uma lista de clientes e imprime seus dados de maneira formatada
+    no terminal.
 
+    :param lista_de_clientes: Lista de clientes a ser impressa.
+                              Cada cliente é representado como um dicionário.
+                              Exemplo: [{"nome": "João", "nif": "123456789", ...}, {...}, ...]
+    """
+    # TODO: Implementar esta função
+    # ...
+
+# Exemplo de uso:
+# lista_de_clientes = [...]  # Substitua isso com sua própria lista de clientes
+# imprime_lista_de_clientes(lista_de_clientes)
